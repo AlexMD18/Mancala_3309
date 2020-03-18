100634156131b374d4e58640f1476b3d746eb0fa
AlexMD18 <alexmd2918@gmail.com>
2020-03-18 11:15:24 -0400

    Update 3/18.2020

---
+++ Mancala/InternalBoardClass.cs
@@ -0,0 +1,39 @@
+﻿/*
+Team: Alex Drogo & Sean Fuller
+Due-Date: 04/02/2020
+CIS 3309-001
+Internal Board Class - 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mancala
+{
+    class InternalBoardClass
+    {
+        GamePlayClass gpc;
+
+        private int[] pocketArray;
+
+
+        //Constructor
+        public InternalBoardClass()
+        {
+            pocketArray = new int[14];
+        }
+
+        public void initalizeBoard()
+        {
+            
+        }
+
+        public bool gameOver()
+        {
+            return true;
+        }
+    }
+}
